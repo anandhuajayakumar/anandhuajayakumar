de81870a96d63e4aa9c0afb838a9d139acb29621
anandhu_ <anandhuaj.yakumar@gmail.com>
2023-10-26 12:32:52 +0530

    code 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextBubbleComponent } from './components/text-bubble/text-bubble.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextBubbleComponent
   ],
   imports: [
     BrowserModule,
